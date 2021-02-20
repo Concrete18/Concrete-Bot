a15431cbb407723bddc7d386df0d3e2c8b436742
Concrete18 <michaelericson19@gmail.com>
2021-02-20 02:15:59 -0500

    fixed return none bugs

--- shared_games_finder.py
+++ shared_games_finder.py
@@ -21,7 +21,6 @@ class Shared_Games:
             for item in data.json()['response']['games']:
                 game_name = item['name']
                 game_list.append(game_name)
-            time.sleep(.5)
             return game_list
         else:
             return 'error'
@@ -59,15 +58,20 @@ class Shared_Games:
                 if games != 'error':
                     lists_to_check.append(self.Get_Game_Names(id))
                     user_check_count += 1
-        if user_check_count < 2:
-            return
+        if user_check_count == 1:
+            return 'Only 1 user is valid'
+        elif user_check_count == 0:
+            return 'No users are valid'
         final_list = self.Check_For_Shared_Games(lists_to_check)
         shared_games = ', '.join(final_list)
-        # print(f'{len(final_list)} shared games found.')
-        # print(shared_games)
-        return shared_games
+        result = f'{len(final_list)} shared games found.\n{shared_games}'
+        return result
 
 
 if __name__ == "__main__":
     App = Shared_Games()
-    App.Create_Game_Lists(['76561197982626192', '76561198088659293', '76561198093285176', '76561198084087457'])
+    overall_start = time.perf_counter() # start time for checking elaspsed runtime
+    print(App.Create_Game_Lists(['76561197982626192', '76561198088659293', '76561198093285176', '76561198084087457']))
+    overall_finish = time.perf_counter() # stop time for checking elaspsed runtime
+    elapsed_time = round(overall_finish-overall_start, 2)
+    print(elapsed_time)
