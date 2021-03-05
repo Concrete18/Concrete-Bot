448fe127516d46f547ee6ad96b6de990553dfd51
Concrete18 <michaelericson19@gmail.com>
2021-03-04 23:12:10 -0500

    moved logging to main bot script

--- functions.py
+++ functions.py
@@ -1,16 +1,6 @@
-from logging.handlers import RotatingFileHandler
-import logging as lg
-
 class bot_functions:
 
     bot_image = 'https://raw.githubusercontent.com/Concrete18/Discord-Bots/master/Images/bot_images.JPG'
-    # Logging
-    log_formatter = lg.Formatter('%(asctime)s %(levelname)s %(message)s', datefmt='%m-%d-%Y %I:%M:%S %p')
-    logger = lg.getLogger(__name__)
-    logger.setLevel(lg.DEBUG) # Log Level
-    my_handler = RotatingFileHandler('bot.log', maxBytes=5*1024*1024, backupCount=2)
-    my_handler.setFormatter(log_formatter)
-    logger.addHandler(my_handler)
 
 
     @staticmethod
