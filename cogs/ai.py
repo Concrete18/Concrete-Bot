44a677508020c5672b97deba95a3bd399c126035
Concrete18 <michaelericson19@gmail.com>
2021-03-12 15:10:30 -0500

    added context and weight

--- cogs/ai.py
+++ cogs/ai.py
@@ -1,12 +1,12 @@
 from discord.ext import commands
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.tokenize import word_tokenize
+import nltk
 import discord as ds
 from functions import *
 import difflib
 import random
 import json
-import sys
 
 
 class AI(commands.Cog):
@@ -17,15 +17,16 @@ class AI(commands.Cog):
         self.bot_func = bot_functions()
         self.stemmer = LancasterStemmer()
         self.valid_channels = [self.bot.bot_commands_test_chan, self.bot.bot_commands_chan]
+        self.last_response_tag = ''
 
         # settings
-        with open("intents.json") as file:
+        with open("ai_data\intents.json") as file:
             self.phrase_data = json.load(file)
         self.similarity_req = self.phrase_data['settings']['similarity_req']
         self.debug = self.phrase_data['settings']['debug']
 
         # load stop words
-        with open("stopwords.txt", "r") as f:
+        with open("ai_data\stopwords.txt", "r") as f:
             self.stopwords = set(f.read())
 
 
@@ -39,14 +40,16 @@ class AI(commands.Cog):
         sentence -- sentence is simplified
         '''
         sentence = self.stemmer.stem(sentence.lower())
-        word_tokens = word_tokenize(sentence)
+        try:
+            word_tokens = word_tokenize(sentence)
+        except:
+            word_tokens = word_tokenize(sentence)
+            nltk.download()
         filtered_sentence = [w for w in word_tokens if not w in self.stopwords]
         filtered_sentence = []
         for w in word_tokens:
             if w not in self.stopwords:
                 filtered_sentence.append(w)
-        if self.debug:
-            print(f'filtered sentence: {filtered_sentence}')
         return filtered_sentence
 
 
@@ -61,23 +64,21 @@ class AI(commands.Cog):
         # switch to giving max_similarity self.similarity_req
         max_similarity = 0
         matched_pattern = ''
-        tag = ''
-        responses = ''
+        best_response = ''
         prepped_phrase = self.simpilify_phrase(phrase)
         for item in intents:
             for pattern in item['patterns']:
                 prepped_pattern = self.simpilify_phrase(pattern)
                 similarity = difflib.SequenceMatcher(None, prepped_pattern, prepped_phrase).ratio()
                 if similarity > max_similarity and similarity > self.similarity_req:
-                    responses = item['responses']
                     max_similarity = similarity
-                    tag = item['tag']
                     matched_pattern = pattern.lower()
-        if responses == '':
-            return None, None, None
+                    best_response = item
+        if best_response == '':
+            return
         if self.debug:
-            print(f'Final pick is: {tag} with similarity: {max_similarity}\n{matched_pattern}\n')
-        return tag, responses, matched_pattern
+            print(f'Final pick is: {best_response["tag"]} with similarity: {max_similarity}\n{matched_pattern}\n')
+        return best_response
 
 
     @commands.Cog.listener()
@@ -85,25 +86,33 @@ class AI(commands.Cog):
         '''
         On message reaction.
         '''
-        print(self.bot.user.id)
-        print(message.content)
+        bot_id = str(812257484734988299)
         if message.author == self.bot.user:  # Ignore messages made by the bot
             return
-        if message.channel.id in self.valid_channels or str(812257484734988299) in message.content:
-            tag, responses, pattern = self.phrase_matcher(message.content)
-            if tag == None:
+        if message.channel.id in self.valid_channels or bot_id in message.content:
+            data_dict = self.phrase_matcher(message.clean_content)
+            if data_dict == None:
                 return
-            if len(responses) > 1:
-                response = random.choice(responses)
-            elif len(responses) == 1:
-                response = responses[0]
+            if 'context_set' in data_dict.keys():
+                if data_dict['context_set'] != self.last_response_tags:
+                    return
+            if 'tag' in data_dict.keys():
+                self.last_response_tags = data_dict['tag']
+            if len(data_dict['responses']) > 1:
+                if 'weighted' in data_dict.keys():
+                    response = random.choices(data_dict['responses'], weights=(data_dict['weighted']))[0]
+                else:
+                    response = random.choice(data_dict['responses'])
+            elif len(data_dict['responses']) == 1:
+                response = data_dict['responses'][0]
             else:
                 print('No responses.')
                 return
             # sends message
             if '@' in response:
                 response = response.replace('@', str(message.author.display_name))
-            await message.channel.send(response)
+            if response != '':
+                await message.channel.send(response)
 
 
 def setup(bot):
