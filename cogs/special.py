d5d7406e832f25bb38ae237cb4b9dfd91a8307f2
Concrete18 <michaelericson19@gmail.com>
2021-09-08 11:44:36 -0400

    commented out no longer needed function

--- cogs/special.py
+++ cogs/special.py
@@ -13,20 +13,20 @@ class Special(commands.Cog):
         self.bot_func = bot_functions()
 
 
-    @commands.command(
-        name='roku',
-        brief='Changes Grandmom\'s TV to ABC.')
-    @commands.is_owner()
-    async def roku(self, ctx):
-        '''
-        Changes Grandmoms TV to ABC.
-        '''
-        script = "D:/Google Drive/Coding/Python/Scripts/1-Complete-Projects/Roku-Control/Instant_Set_to_ABC.py"
-        if os.path.exists(script):
-            subprocess.run([sys.executable, script], cwd=os.path.dirname(script))
-            await ctx.send('Roku Channel Change was started.')
-        else:
-            await ctx.send('Path does not exist anymore.')
+    # @commands.command(
+    #     name='roku',
+    #     brief='Changes Grandmom\'s TV to ABC.')
+    # @commands.is_owner()
+    # async def roku(self, ctx):
+    #     '''
+    #     Changes Grandmoms TV to ABC.
+    #     '''
+    #     script = "D:/Google Drive/Coding/Python/Scripts/1-Complete-Projects/Roku-Control/Instant_Set_to_ABC.py"
+    #     if os.path.exists(script):
+    #         subprocess.run([sys.executable, script], cwd=os.path.dirname(script))
+    #         await ctx.send('Roku Channel Change was started.')
+    #     else:
+    #         await ctx.send('Path does not exist anymore.')
 
 
 def setup(bot):
